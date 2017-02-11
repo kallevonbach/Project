9a53332a3b19881eb81f31ba89c7b50ed96b413f
kallevonbach <kallevonbach@gmail.com>
2017-02-12 00:45:04 +0100

    frontpage, contactpage done, started on detailpage

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ jQuery(document).ready(function(){
       'controls': false,
       'slideWrapper': '<div class="sy-slides-wrap" />'
   })
+    $("#nav-mobile").html($(".nav-main").html());
+    $("#nav-trigger span").click(function(){
+        if ($("nav#nav-mobile ul").hasClass("expanded")) {
+            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
 });
